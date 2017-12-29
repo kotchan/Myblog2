4cd0d2cdd1788455ced021b7b2328a265512ea10
Kotoyo Nozaki <kotokoto3726@asagi.waseda.jp>
2017-12-29 16:31:42 +0000

    Add root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
+  root 'message_test2s#index'
   resources :message_test2s
-  resources :message2s
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
